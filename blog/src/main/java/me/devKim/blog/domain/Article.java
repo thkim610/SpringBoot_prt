f9fb4896247824f308fa6a00425ec20c98f3f139
thkim610 <thkim610@naver.com>
2023-10-18 22:42:55 +0900

    [mod]:블로그 글 수정을 위한 엔티티 수정(Article) #5

--- blog/src/main/java/me/devKim/blog/domain/Article.java
+++ blog/src/main/java/me/devKim/blog/domain/Article.java
@@ -28,5 +28,11 @@ public class Article {
         this.content = content;
     }
 
+    //블로그 글 수정 기능
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
 
 }
