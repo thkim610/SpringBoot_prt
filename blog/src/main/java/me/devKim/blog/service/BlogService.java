998be4253527d9361194abe585adaab405c4ba26
thkim610 <thkim610@naver.com>
2023-10-18 18:15:52 +0900

    [add]:Service - findAll() 구현 #2

--- blog/src/main/java/me/devKim/blog/service/BlogService.java
+++ blog/src/main/java/me/devKim/blog/service/BlogService.java
@@ -7,6 +7,8 @@ import me.devKim.blog.dto.AddArticleRequest;
 import me.devKim.blog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @RequiredArgsConstructor //final 키워드가 붙거나 @Not null이 붙은 필드의 생성자 추가.
 @Service //빈으로 등록
@@ -14,8 +16,14 @@ public class BlogService {
 
     private final BlogRepository blogRepository;
 
+    //블로그 글 작성
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    //블로그 글 목록 조회
+    public List<Article> findAll(){
+        return blogRepository.findAll(); //jpa레포에서 지원하는 findAll메서드 호출
     }
 
 
