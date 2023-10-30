d9b59a668e3b38b52e5bc658a0263d8f2725462a
thkim610 <thkim610@naver.com>
2023-10-30 20:16:05 +0900

    [mod]:JS - 생성 기능 추가 (article.js) #9

--- blog/src/main/resources/static/js/article.js
+++ blog/src/main/resources/static/js/article.js
@@ -19,26 +19,49 @@ if(deleteButton){
 //블로그 글 수정 기능
 const modifyButton = document.getElementById('modify-btn');
 
-if(modifyButton){
+if(modifyButton) {
     // 클릭 이벤트가 감지되면 수정 API 요청
-    modifyButton.addEventListener('click', event=>{
+    modifyButton.addEventListener('click', event => {
         let params = new URLSearchParams(location.search);
         let id = params.get('id'); //파라미터에서 id 값을 id에 할당.
         //수정API요청 (fetch/PUT) => JSON 방식으로 통신
-        fetch(`/api/articles/${id}`,{
-            method : 'PUT',
-            headers : {
-                "Content-Type" : "application/json", //json 형식으로 데이터 전송
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json", //json 형식으로 데이터 전송
             },
-            body : JSON.stringify({ //json 형식으로 파라미터의 값을 서버로 전송한다.
+            body: JSON.stringify({ //json 형식으로 파라미터의 값을 서버로 전송한다.
                 title: document.getElementById('title').value,
                 content: document.getElementById('content').value
             })
         })
-            .then(()=>{
+            .then(() => {
                 alert('수정이 완료되었습니다.'); //요청 처리 후 완료 응답으로 알림창 띄움.
                 location.replace(`/articles/${id}`); //해당 id의 블로그 글로 이동.
             });
     });
+}
+
+    //블로그 글 생성 기능
+    const createBtn = document.getElementById('create-btn');
+    console.log(createBtn);
+    if(createBtn){
+        //클릭 이벤트가 감지되면 생성 API 요청
+        createBtn.addEventListener('click', event=>{
+            //생성API요청 (fetch/POST) => JSON 방식으로 통신
+            fetch("/api/articles", {
+                method: 'POST',
+                headers: {
+                    "Content-Type" : "application/json", //json 형식으로 데이터 전송
+                },
+                body: JSON.stringify({
+                    title: document.getElementById('title').value,
+                    content: document.getElementById('content').value,
+                }),
+            }).then(()=>{
+                    alert('등록이 완료되었습니다.');
+                    location.replace("/articles");
+                });
+        });
 
 }
